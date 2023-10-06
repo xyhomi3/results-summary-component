16af69c30dace2692e5bc8941abc77897f09af9b
XYHub <lucien.loua@gmail.com>
2023-10-06 20:33:32 +0000

    redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import * as path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/',
+  base:'./',
   resolve:{
     alias:{
       '@src' : path.resolve(__dirname, 'src')
