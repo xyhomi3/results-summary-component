e1d20037c510dae256fa1a3a5ed595ba2e16d398
XYHub <lucien.loua@gmail.com>
2023-10-06 18:03:03 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Frontend Mentor - Results summary component solution
+
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [The Challenge](#the-challenge)
+  - [Links](#links)
+- [Built With](#built-with)
+- [How to Use](#how-to-use)
+- [Author](#author)
+
+## Overview
+
+### The Challenge
+
+Users should be able to:
+
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: Use the local JSON data to dynamically populate the content
+
+### Links
+
+- [Solution URL](https://www.frontendmentor.io/solutions/interactivecarddetailsform-wbunvitereacttypescipt-2NxVVbuQfm)
+
+- [Live Site URL](https://xyhomi3.github.io/interactive-card-details-form/)
+
+### Built With
+
+- CSS
+- Grid Design Style
+- [React](https://reactjs.org/) - Js library
+- [Vite.js](https://vitejs.dev/) - React framework
+- [Bun](https://bun.sh/) - Js Runtime
+
+## How to Use
+
+1. Clone the repository: `git clone https://github.com/your-username/interactive-card-details-form.git`
+2. Install dependencies: `bun install`
+3. Start the development server: `bun run dev`
+4. Open your web browser and visit `http://localhost:5173` to view the application.
+
+## Author
+
+- XYHOMi3
+- GitHub: [@xyhomi3](https://www.github.com/xyhomi3)
+- Twitter: [@xyhomi3](https://www.twitter.com/xyhomi3)
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/xyhomi3)
---
+++ src/app/App.tsx
@@ -0,0 +1,114 @@
+function App() {
+  const summaryData = [
+    {
+      category: "Reaction",
+      score: 80,
+      icon: "./src/assets/images/icon-reaction.svg",
+    },
+    {
+      category: "Memory",
+      score: 92,
+      icon: "./src/assets/images/icon-memory.svg",
+    },
+    {
+      category: "Verbal",
+      score: 61,
+      icon: "./src/assets/images/icon-verbal.svg",
+    },
+    {
+      category: "Visual",
+      score: 72,
+      icon: "./src/assets/images/icon-visual.svg",
+    },
+  ];
+
+  return (
+    <>
+      <div className="card">
+        <div className="result">
+          <div
+            style={{
+              display: "flex",
+              placeSelf: "center",
+            }}
+          >
+            <h1 className="h1" style={{ color: "var(--Light-lavender)" }}>Your Result</h1>
+          </div>
+          <div className="result-data">
+            <div className="circle-gradient">
+              <h1
+                style={{
+                  fontSize: "3.5em",
+                  position: "absolute",
+                  paddingBottom: "0.3em",
+                }}
+              >
+                76
+              </h1>
+              <p className="p">
+                of 100
+              </p>
+            </div>
+            <div className="comments" style={{paddingTop:"0em"}}>
+              <h1 >
+                Great
+              </h1>
+              <p style={{ color: "var(--Light-lavender)" }}>
+                You scored higher than 65% of the<br /> people who have taken
+                these tests.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        {/* Summary Section */}
+        <div className="summary">
+          <div>
+            <h1>Summary</h1>
+          </div>
+
+          <ul className="summary-data">
+            {summaryData.map((item, index) => (
+              <li key={index} className="data" id={`${item.category}`}>
+                <div style={{ display: "flex", gap: "0.7em" }}>
+                  <img
+                    src={item.icon}
+                    alt={`${item.category} icon`}
+                    className="icon"
+                  />
+                  <span className={`${item.category}`}> {item.category}</span>
+                </div>
+                <span className="score">
+                  {item.score}
+                  {
+                    <span style={{ color: "var(--Light-lavender)" }}>
+                      {" "}
+                      {"/ 100"}
+                    </span>
+                  }
+                </span>
+              </li>
+            ))}
+          </ul>
+
+          <div className="space">
+            <button>Continue</button>
+          </div>
+
+          <div className="attribution">
+            Challenge by{" "}
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+            >
+              Frontend Mentor
+            </a>
+            . Coded by <a href="#">XYHOMi3</a>.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
